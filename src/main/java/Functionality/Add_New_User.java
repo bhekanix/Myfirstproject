1d37fea42b5bc70d0712fe594ff2490971fcd3ad
Xolani <bhekanix@gmail.com>
2020-04-27 14:01:03 +0200

    Working project first project

--- src/main/java/Functionality/Add_New_User.java
+++ src/main/java/Functionality/Add_New_User.java
@@ -4,21 +4,155 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 public class Add_New_User {
     private WebDriver driver;
-    public  Add_New_User (WebDriver driver)
-    {
+    public Add_New_User(WebDriver driver) {
         this.driver = driver;
 
     }
+
     @FindBy(xpath = "//button[contains(.,'Add User')]")
     WebElement AddUserButton;
-    public void Add_User_Button(){
-        WebDriverWait adduserbutton = new WebDriverWait(driver,20);
+    @FindBy(xpath = "//button[contains(.,'Save')]")
+    WebElement SaveUserButton;
+    @FindBy(xpath = "//input[@name='FirstName']")
+    WebElement FirstName;
+    @FindBy(xpath = "//input[@name='LastName']")
+    WebElement LastName;
+    @FindBy(xpath = "//input[contains(@name,'UserName')]")
+    WebElement UserName;
+    @FindBy(xpath = "//input[contains(@type,'password')]")
+    WebElement Password;
+    @FindBy(xpath = "//input[@value='15']")
+    WebElement CompanyA;
+    @FindBy(xpath = "//input[contains(@value,'16')]")
+    WebElement CompanyB;
+    @FindBy(xpath = "//select[contains(@name,'RoleId')]")
+    WebElement Role;
+    @FindBy(xpath = "//input[contains(@type,'email')]")
+    WebElement Email;
+    @FindBy(xpath = "//input[contains(@name,'Mobilephone')]")
+    WebElement CellPhone;
+    @FindBy(xpath = "(//td[@class='smart-table-data-cell'])[1]")
+    WebElement SavedFirstname;
+    @FindBy(xpath = "//button[contains(.,'Close')]")
+    WebElement CloseUserButton;
+    @FindBy(xpath = "(//button[contains(.,'Edit')])[1]")
+    WebElement EditButton;
+    @FindBy(xpath = "(//input[@name='IsLocked'])[8]")
+    WebElement IsLocked;
+
+    public void Add_User_Button() {
+        WebDriverWait adduserbutton = new WebDriverWait(driver, 20);
         adduserbutton.until(ExpectedConditions.visibilityOf(AddUserButton));
         AddUserButton.click();
 
+
     }
+//    public void Edit_User_Button() {
+//        WebDriverWait edituserbutton = new WebDriverWait(driver, 20);
+//        edituserbutton.until(ExpectedConditions.visibilityOf(EditButton));
+//        EditButton.click();
+//
+//
+//    }
+//    public void Locked_Button() {
+//        WebDriverWait lockbutton = new WebDriverWait(driver, 20);
+//        lockbutton.until(ExpectedConditions.visibilityOf(IsLocked));
+//        IsLocked.click();
+//
+//
+//    }
+
+
+
+    public void Enter_Name(String _firstname) {
+        WebDriverWait adduserbutton = new WebDriverWait(driver, 20);
+        adduserbutton.until(ExpectedConditions.visibilityOf(FirstName));
+        FirstName.clear();
+        FirstName.sendKeys(_firstname);
+
+
+    }
+
+    public void Enter_Lastname(String _lastname) {
+        WebDriverWait adduserbutton = new WebDriverWait(driver, 20);
+        adduserbutton.until(ExpectedConditions.visibilityOf(LastName));
+        LastName.clear();
+        LastName.sendKeys(_lastname);
+    }
+
+    public void Enter_Username(String _username) {
+        WebDriverWait adduserbutton = new WebDriverWait(driver, 20);
+        adduserbutton.until(ExpectedConditions.visibilityOf(UserName));
+        UserName.clear();
+        UserName.sendKeys(_username);
+
+    }
+
+    public void Enter_Password(String _password) {
+        WebDriverWait adduserbutton = new WebDriverWait(driver, 20);
+        adduserbutton.until(ExpectedConditions.visibilityOf(Password));
+        Password.sendKeys(_password);
+    }
+
+    public void Enter_Email(String _email) {
+        WebDriverWait adduserbutton = new WebDriverWait(driver, 20);
+        adduserbutton.until(ExpectedConditions.visibilityOf(Email));
+        Email.clear();
+        Email.sendKeys(_email);
+    }
+
+    public void Enter_Cellphone(String _cellphone) {
+        WebDriverWait adduserbutton = new WebDriverWait(driver, 20);
+        adduserbutton.until(ExpectedConditions.visibilityOf(CellPhone));
+        CellPhone.clear();
+        CellPhone.sendKeys(_cellphone);
+    }
+
+    public void Select_Role(String _role) {
+        WebDriverWait adduserbutton = new WebDriverWait(driver, 20);
+        adduserbutton.until(ExpectedConditions.visibilityOf(Role));
+        Select drp = new Select(Role);
+        drp.selectByVisibleText("Customer");
+    }
+
+    public void Select_Company(String _Company) {
+        WebDriverWait adduserbutton = new WebDriverWait(driver, 20);
+        adduserbutton.until(ExpectedConditions.visibilityOf(CompanyA));
+        if (_Company.equalsIgnoreCase("CompanyAAA")) {
+            CompanyA.click();
+
+        } else if (_Company.equalsIgnoreCase("CompanyBBB")) {
+            CompanyB.click();
+        }
+
+    }
+
+    public void Save_New_user() {
+        WebDriverWait saveuserbutton = new WebDriverWait(driver, 20);
+        saveuserbutton.until(ExpectedConditions.visibilityOf(SaveUserButton));
+        SaveUserButton.click();
+    }
+
+    public void Verify_Added_User(String _name) {
+        WebDriverWait verifyaddeduser = new WebDriverWait(driver, 20);
+        verifyaddeduser.until(ExpectedConditions.visibilityOf(SavedFirstname));
+        Assert.assertEquals(SavedFirstname.getText(), _name);
+    }
+
+    public void Close_User_Button() {
+        WebDriverWait closeuserbutton = new WebDriverWait(driver, 20);
+        closeuserbutton.until(ExpectedConditions.visibilityOf(CloseUserButton));
+        CloseUserButton.click();
+
+    }
+
 }
+
+
+
--- src/main/java/Test/Add_And_Verify_User_Tests.java
+++ src/main/java/Test/Add_And_Verify_User_Tests.java
@@ -2,26 +2,100 @@ package Test;
 
 import Functionality.Add_New_User;
 import Setup.Setup_Class;
+import freemarker.ext.jsp.TaglibFactory;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import sun.plugin.dom.core.Comment;
+import sun.plugin.dom.core.Document;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 
 public class Add_And_Verify_User_Tests {
-    private WebDriver driver = Setup_Class.startBrowserOfChoice("http://www.way2automation.com/angularjs-protractor/webtables/","chrome");
+    private WebDriver driver = Setup_Class.startBrowserOfChoice("http://www.way2automation.com/angularjs-protractor/webtables/", "chrome");
     private static String dir = System.getProperty("user.dir");
+    private static final String excel = dir + "/src/main/java/Test_data/testdata.xlsx";
+
     public Add_And_Verify_User_Tests() throws InterruptedException {
     }
 
     @Test()
-    public void Add_User(){
-        Add_New_User myuser = PageFactory.initElements(driver,Add_New_User.class);
-        myuser.Add_User_Button();
+    public void Save_User() throws IOException {
+        for (int i = 1; i < 10; i++) {
+
+            FileInputStream fis = new FileInputStream(excel);
+            XSSFWorkbook workbook = new XSSFWorkbook(fis);
+            XSSFSheet sheet = workbook.getSheetAt(0);
+            String first_name = sheet.getRow(i).getCell(0).getStringCellValue();
+            String Last_name = sheet.getRow(i).getCell(1).getStringCellValue();
+            String User_name = sheet.getRow(i).getCell(2).getStringCellValue();
+            String Password = sheet.getRow(i).getCell(3).getStringCellValue();
+            String Company = sheet.getRow(i).getCell(4).getStringCellValue();
+            String Role = sheet.getRow(i).getCell(5).getStringCellValue();
+            String Email = sheet.getRow(i).getCell(6).getStringCellValue();
+            String Cell_phone = sheet.getRow(i).getCell(7).getStringCellValue();
+            Add_New_User saveuser = PageFactory.initElements(driver, Add_New_User.class);
+            Add_New_User myuser = PageFactory.initElements(driver, Add_New_User.class);
+            myuser.Add_User_Button();
+            saveuser.Enter_Name(first_name);
+            saveuser.Enter_Lastname(Last_name);
+            saveuser.Enter_Username(User_name);
+            saveuser.Enter_Password(Password);
+            saveuser.Select_Company(Company);
+            saveuser.Select_Role(Role);
+            saveuser.Enter_Email(Email);
+            saveuser.Enter_Cellphone(Cell_phone);
+            saveuser.Save_New_user();
+            saveuser.Verify_Added_User(first_name);
+
+
+        }
+
+
     }
+//    @Test()
+//    public void Edit_Button(){
+//        Add_New_User edituser = PageFactory.initElements(driver,Add_New_User.class);
+//        Add_New_User saveuser = PageFactory.initElements(driver, Add_New_User.class);
+//        Add_New_User lockuser = PageFactory.initElements(driver, Add_New_User.class);
+//        edituser.Edit_User_Button();
+//        lockuser.Locked_Button();
+//        saveuser.Save_New_user();
+//
+//    }
+
+    @AfterTest()
+    public void Quit()
+    {
+        driver.quit();
+    }
+    @Test()
+    public void Close_Form() {
+        Add_New_User closeuser = PageFactory.initElements(driver, Add_New_User.class);
+        Add_New_User myuser = PageFactory.initElements(driver, Add_New_User.class);
+        myuser.Add_User_Button();
+        closeuser.Close_User_Button();
 
 
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
